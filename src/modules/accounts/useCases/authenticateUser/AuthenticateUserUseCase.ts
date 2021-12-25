f3023f9b7f87748d1b597369d5e58e3930245393
Leonardo <leonardobarrochello58@gmail.com>
2021-12-25 00:13:48 -0300

    refatoração das importações

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,9 +1,8 @@
+import AppError from "@errors/AppError";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
-
-import AppError from "../../../../errors/AppError";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IResponse {
     user: {
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import "reflect-metadata";
+import AppError from "@errors/AppError";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
-import "./shared/container";
+import "@shared/container";
 
-import AppError from "./errors/AppError";
 import { router } from "./routes";
 import swaggerDocument from "./swagger.json";
 
